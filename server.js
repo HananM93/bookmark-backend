28103755dd878fa361a25c4a096124718bf8bf5d
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-01-04 23:01:15 -0500

    KM first contribution

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const { PORT = 3001 } = process.env
 const express = require("express")
 // create application object
 const app = express()
+const mongoose = require('mongoose')
 
+// DATABASE CONNECTION
+mongoose.connect(process.env.DATABASE_URL, {})
+
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+// MODEL
+const bookmarkSchema = new mongoose.Schema({
+  title: {type: String},
+  url: {type: String},
+})
+
+const Bookmark = mongoose.model('bookmark', bookmarkSchema)
 
 // ROUTES IDUC
 
@@ -17,6 +33,28 @@ const app = express()
 app.get("/", (req, res) => {
   res.send("hello world")
 })
+
+// INDEX - all bookmarks
+app.get('/bookmarks', (req, res) => {
+  res.send('These sites are bookmarked')
+})
+
+// DELETE 
+app.delete('/bookmarks/:id', (req, res) => {
+  
+})
+
+// UPDATE
+app.put('/bookmarks/:id', (req, res) => {
+
+})
+
+// CREATE
+app.post('/bookmarks', (req, res) => {
+
+})
+
+
 
 
 // LISTENER
